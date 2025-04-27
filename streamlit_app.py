9116da7b14230556c07dd2a5a43ca0e416f5f1ff
FluxNSFW_AI <nsfwflux@gmail.com>
2025-04-27 21:20:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Use the best [AI NSFW Image Generator](https://fluxnsfw.ai), powered by [Flux with no restrictions](https://fluxnsfw.ai)."
 )
